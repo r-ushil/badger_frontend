9504c7857d0cf78379006c6fad4f25fde1fc5ffa
r-ushil <rushil.patel791@gmail.com>
2022-10-30 19:46:36 +0000

    feat: added theming to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
+import 'package:badger_frontend/dashboard/view-model/dashboard_view_model.dart';
 import 'package:badger_frontend/dashboard/view/dashboard.dart';
+import 'package:badger_frontend/styles/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Dashboard()));
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<DashboardViewModel>(create: (context) => DashboardViewModel())
+      ],
+      child: MaterialApp(
+        theme: getThemeData(),
+        home: const Dashboard(),
+      ),
+    );
+  }
 }
