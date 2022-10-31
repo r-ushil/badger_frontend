f5da3a0df25b5d0a0eb5a818a2a77456d09ecfc5
r-ushil <rushil.patel791@gmail.com>
2022-10-31 17:20:02 +0000

    feat: added videoplayer element state and dispose elements

--- lib/video_cv/view/video_cv.dart
+++ lib/video_cv/view/video_cv.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoCV extends StatefulWidget {
   const VideoCV({Key? key}) : super(key: key);
@@ -8,17 +9,51 @@ class VideoCV extends StatefulWidget {
 }
 
 class _VideoCVState extends State<VideoCV> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://drive.google.com/file/d/1qdUiELWFjI8A3rzCrEJFy6j4K3nWX7bv/view?usp=drivesdk')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Video CV'),
       ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const <Widget>[
-
-          ]),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
