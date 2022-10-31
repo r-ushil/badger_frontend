fcbce5540eec16cf25c68cc34d631cb2bc6e8bcc
Oli Callaghan <me@olicallaghan.com>
2022-10-31 14:54:51 +0530

    feat: switch Firebase project to seg-badger-dev

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,21 @@ import 'package:badger_frontend/styles/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const App());
+import "common/firebase/firebase.dart";
+
+void main() async {
+  /* Initialise widgets library */
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /* Initialise plugins */
+  await initializeFirebase();
+
+  /* Bootstrap Flutter App */
+  runApp(const RootApp());
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class RootApp extends StatelessWidget {
+  const RootApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
