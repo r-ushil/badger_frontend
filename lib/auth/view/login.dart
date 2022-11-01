d5dcea2f3d832a497a1869c8d9ff51c5cbc45489
Viktoria Adorjan <va520@ic.ac.uk>
2022-11-01 13:20:28 +0000

    refactor: move functionality of logo into new Logo class

--- lib/auth/view/login.dart
+++ lib/auth/view/login.dart
@@ -2,6 +2,7 @@ import 'package:badger_frontend/dashboard/view/widgets/metric_chart.dart';
 import 'package:badger_frontend/dashboard/view/widgets/progress_bars.dart';
 import 'package:badger_frontend/drill_list/view/drill_list.dart';
 import 'package:flutter/material.dart';
+import 'package:badger_frontend/auth/view/widgets/logo.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -17,10 +18,10 @@ class _LoginState extends State<Login> {
       appBar: AppBar(),
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Image.asset("images/logo.png", height: 200, width: 200,),
-            const SizedBox(height: 50), // margin between logo and welcome text
-            const Text('WELCOME', textAlign: TextAlign.center, style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+          children: const <Widget>[
+            Logo(),
+            SizedBox(height: 50), // margin between logo and welcome text
+            Text('WELCOME', textAlign: TextAlign.center, style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
           ]),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
