33c91b2d93e2fe324056ac544ef1d1ff037231bd
Viktoria Adorjan <va520@ic.ac.uk>
2022-11-01 13:06:55 +0000

    feat: added logo and welcome text to login page

--- lib/auth/view/login.dart
+++ lib/auth/view/login.dart
@@ -14,14 +14,13 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome!'),
-      ),
+      appBar: AppBar(),
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const <Widget>[
-            MetricChart(),
-            MetricProgressBars(),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("images/logo.png", height: 200, width: 200,),
+            const SizedBox(height: 50), // margin between logo and welcome text
+            const Text('WELCOME', textAlign: TextAlign.center, style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
           ]),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
