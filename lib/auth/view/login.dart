be8622978e0e0e2842593fa8e9550a2d194df3c9
Viktoria Adorjan <va520@ic.ac.uk>
2022-11-02 11:50:31 +0000

    feat: added login button

--- lib/auth/view/login.dart
+++ lib/auth/view/login.dart
@@ -1,3 +1,4 @@
+import 'package:badger_frontend/dashboard/view/dashboard.dart';
 import 'package:badger_frontend/dashboard/view/widgets/metric_chart.dart';
 import 'package:badger_frontend/dashboard/view/widgets/progress_bars.dart';
 import 'package:badger_frontend/drill_list/view/drill_list.dart';
@@ -6,6 +7,7 @@ import 'package:badger_frontend/auth/view/widgets/logo.dart';
 import 'package:badger_frontend/auth/view/widgets/welcome_header.dart';
 import 'package:badger_frontend/auth/view/widgets/phone_textbox.dart';
 import 'package:badger_frontend/auth/view/widgets/text_button.dart';
+import 'package:badger_frontend/auth/view/widgets/custom_button.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -21,28 +23,16 @@ class _LoginState extends State<Login> {
       appBar: AppBar(),
       //resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(child: Column(
-          children: <Widget>[
-            const Center(child: Logo()),
-            const WelcomeHeader(),
-            const SizedBox(height: 50), // margin welcome text
-            const TextBoxWithLabelAndIcon("Phone Number", Icons.phone),
-            const TextBoxWithLabelAndIcon("Password", Icons.lock),
-            const CustomTextButton("Forgot your Password?"),
-            const SizedBox(height: 20),
-            SizedBox(width: 300,
-            child: TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  foregroundColor: Colors.white
-              ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const DrillList()));
-              },
-              child: const Text("Login"),
-            )
-            ),
-            const CustomTextButton("Need an account? SIGNUP"),
+          children: const <Widget>[
+            Center(child: Logo()),
+            WelcomeHeader(),
+            SizedBox(height: 50), // margin welcome text
+            TextBoxWithLabelAndIcon("Phone Number", Icons.phone),
+            TextBoxWithLabelAndIcon("Password", Icons.lock),
+            CustomTextButton("Forgot your Password?"),
+            SizedBox(height: 20),
+            CustomButton("Login", Dashboard()),
+            CustomTextButton("Need an account? SIGNUP"),
           ])),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
