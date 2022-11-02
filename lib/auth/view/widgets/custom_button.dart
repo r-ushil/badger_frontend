1b1e191744b9049c12e5c65bf0d342d952d8a6ff
Viktoria Adorjan <va520@ic.ac.uk>
2022-11-02 11:50:31 +0000

    feat: added login button

---
+++ lib/auth/view/widgets/custom_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:badger_frontend/drill_list/view/drill_list.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(this.text, this.goto, {Key? key}) : super(key: key);
+
+  final String text;
+  final Widget goto;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: 300,
+        child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white
+          ),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => goto));
+          },
+          child: Text(text),
+        )
+    );
+  }
+}
