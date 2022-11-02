1afcd00ca6774b2510699a39251b0372d07457dc
okarz <oka20@ic.ac.uk>
2022-11-02 00:26:26 +0000

    feat: added state machine to track user set up progress and display instructions

--- lib/ar/view/ar.dart
+++ lib/ar/view/ar.dart
@@ -11,6 +11,18 @@ import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:ar_flutter_plugin/widgets/ar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart';
+import 'package:statemachine/statemachine.dart' as state;
+import 'package:flutter/services.dart';
+
+enum UserState {
+  Sart,
+  OneCone,
+  TwoCones,
+  TooClose,
+  TooFar,
+  ConfirmCones,
+  AllignCones
+}
 
 class AR extends StatefulWidget {
   const AR({Key? key}) : super(key: key);
@@ -24,6 +36,8 @@ class _ARState extends State<AR> {
   late ARObjectManager arObjectManager;
   late ARAnchorManager arAnchorManager;
 
+  state.Machine<UserState> sMachine = state.Machine<UserState>();
+
   double coneDistance = 0;
 
   ARNode? cone1Node;
@@ -31,25 +45,85 @@ class _ARState extends State<AR> {
   ARNode? cone2Node;
   ARAnchor? cone2Anchor;
 
+  bool confirmButtonVisibility = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(coneDistance.toString())),
-        body: Column(
-          children: [
-            Expanded(
-                child: ARView(
-                  onARViewCreated: onARViewCreated,
-                  planeDetectionConfig: PlaneDetectionConfig.horizontal,
-                )
-            ),
-            Text("hello")
-          ]
+        body: OrientationBuilder(
+          builder: (context, orientation)
+          => orientation == Orientation.portrait ? buildPortrait()
+                                                 : buildLandscape(),
         )
     );
   }
 
+  Widget buildPortrait() {
+    return Column(
+        children: [
+          Expanded(
+              child: ARView(
+                onARViewCreated: onARViewCreated,
+                planeDetectionConfig: PlaneDetectionConfig.horizontal,
+              )
+          ),
+          Text(getInstruction(sMachine)),
+          Visibility(
+            visible: confirmButtonVisibility,
+              child: IconButton(onPressed: () async {
+                await SystemChrome.setPreferredOrientations([
+                  DeviceOrientation.landscapeRight,
+                  DeviceOrientation.landscapeLeft,
+                ]);
+                sMachine.current = UserState.AllignCones;
+
+              }, icon: const Icon(Icons.check_circle, color: Color(0x0000FFc8),))
+          )
+        ]
+    );
+  }
+
+  Widget buildLandscape() {
+    return buildPortrait();
+  }
+
+  String getInstruction(state.Machine<UserState> machine) {
+    if (machine.current == null) {
+      return "";
+    }
+
+    var uState = machine.current?.identifier;
+
+    if (uState == UserState.Sart) {
+      return "Place your first cone down";
+    } else if (uState == UserState.OneCone) {
+      return "Place your second cone down";
+    } else if (uState  == UserState.TooFar) {
+      return "Move cones closer";
+    } else if (uState == UserState.TooClose){
+      return "Move cones further away";
+    } else {
+      return "Done";
+    }
+  }
+
   void onARViewCreated(ARSessionManager arSessionManager, ARObjectManager arObjectManager, ARAnchorManager arAnchorManager, ARLocationManager arLocationManager) {
+    for (UserState uState in UserState.values) {
+      var s = sMachine.newState(uState);
+      if (uState == UserState.ConfirmCones) {
+        s.onEntry(() {
+          confirmButtonVisibility = true;
+          setState(() {});
+        });
+        s.onExit(() {
+          confirmButtonVisibility = false;
+          setState(() {});
+        });
+      }
+    }
+    sMachine.start();
+    setState(() {});
     this.arSessionManager = arSessionManager;
     this.arObjectManager = arObjectManager;
     this.arAnchorManager = arAnchorManager;
@@ -64,8 +138,14 @@ class _ARState extends State<AR> {
     this.arObjectManager.onPanChange = onPanChanged;
   }
 
+  double round1(double x) {
+    return double.parse(x.toStringAsFixed(1));
+  }
+
   Future<void> onPlaneOrPointTapped(List<ARHitTestResult> results) async {
-    if (cone1Anchor != null && cone2Anchor != null) {
+    var uState = sMachine.current?.identifier;
+
+    if (!(uState == UserState.Sart || uState == UserState.OneCone)) {
       return;
     }
     ARHitTestResult result = results.firstWhere((r) => r.type == ARHitTestResultType.plane);
@@ -74,44 +154,67 @@ class _ARState extends State<AR> {
     if (added == null || added == false) {
       return;
     }
-    if (cone1Anchor == null) {
-      cone1Anchor = anchor;
-    } else {
-      cone2Anchor = anchor;
-    }
 
     var node = ARNode(
         type: NodeType.webGLB,
         uri:
         "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb",
         scale: Vector3(0.2, 0.2, 0.2),
-        position: Vector3(0.0, 0.0, 0.0),
+        position: Vector3(0, 0, 0),
         rotation: Vector4(1.0, 0.0, 0.0, 0.0));
     added = await arObjectManager.addNode(node, planeAnchor: anchor);
     if (added == null || added == false) {
       return;
     }
-    if (cone1Node == null) {
+    if (uState == UserState.Sart) {
+      cone1Anchor = anchor;
       cone1Node = node;
+      sMachine.current = UserState.OneCone;
+      setState(() {});
     } else {
+      cone2Anchor = anchor;
       cone2Node = node;
-    }
+      coneDistance = round1((await arSessionManager.getDistanceBetweenAnchors(cone1Anchor!, cone2Anchor!))!);
+      if (coneDistance < 1.0) {
+        sMachine.current = UserState.TooClose;
+      }  else if (coneDistance > 1.0)  {
+        sMachine.current = UserState.TooFar;
+      } else {
+        sMachine.current = UserState.ConfirmCones;
+        await SystemChrome.setPreferredOrientations([
+          DeviceOrientation.landscapeRight,
+          DeviceOrientation.landscapeLeft,
+        ]);
 
+      }
+
+      setState(() {});
+    }
   }
 
   onPanChanged(String nodeName) async {
-    if (cone1Anchor == null || cone2Anchor == null) {
+    var uState = sMachine.current?.identifier;
+    if (uState == UserState.Sart || uState == UserState.OneCone) {
       return;
     }
-    coneDistance = (await arSessionManager.getDistanceBetweenAnchors(cone1Anchor!, cone2Anchor!))!;
-    setState(() {
-
-    });
+    coneDistance = round1((await arSessionManager.getDistanceBetweenAnchors(cone1Anchor!, cone2Anchor!))!);
+    if (coneDistance < 1) {
+      sMachine.current = UserState.TooClose;
+    } else if (coneDistance > 1) {
+      sMachine.current = UserState.TooFar;
+    } else {
+      sMachine.current = UserState.ConfirmCones;
+    }
+    setState(() {});
   }
 
   @override
-  void dispose() {
+  Future<void> dispose() async {
     super.dispose();
     arSessionManager.dispose();
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
   }
 }
