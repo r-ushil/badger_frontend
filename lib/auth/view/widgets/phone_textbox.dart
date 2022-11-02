c786bfaeb2ea9d6a58743a00b2a1f72ff20e8615
Viktoria Adorjan <va520@ic.ac.uk>
2022-11-02 11:18:06 +0000

    feat: added Password textbox

--- lib/auth/view/widgets/phone_textbox.dart
+++ lib/auth/view/widgets/phone_textbox.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
-class PhoneTextbox extends StatelessWidget {
-  const PhoneTextbox({Key? key}) : super(key: key);
+class TextBoxWithLabelAndIcon extends StatelessWidget {
+  const TextBoxWithLabelAndIcon(this.title, this.icon, {super.key});
+
+  final String title;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: const <Widget>[
-          Padding(padding: EdgeInsets.only(right: 150.0),
-              child: Text("Phone Number",
-                           style: TextStyle(fontSize: 20,
+        children: <Widget>[
+          Padding(padding: const EdgeInsets.only(right: 150.0),
+              child: Text(title,
+                           style: const TextStyle(fontSize: 20,
                                             color: Colors.grey
                            )
               )
           ),
-          SizedBox(height: 10), // margin before textbox
+          const SizedBox(height: 10), // margin before textbox
           SizedBox(width: 300,
               child: TextField(
-                  style: TextStyle(color: Colors.grey),
-                  decoration: InputDecoration(prefixIcon: Icon(Icons.phone,
+                  style: const TextStyle(color: Colors.grey),
+                  decoration: InputDecoration(prefixIcon: Icon(icon,
                                                           color: Colors.grey),
                                               filled: true,
                                               fillColor: Colors.white12
