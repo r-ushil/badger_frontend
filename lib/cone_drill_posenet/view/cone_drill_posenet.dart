ea1034d983e7b02f5e1157ec8b104478abac3289
Otto White <whiteotto4@gmail.com>
2022-11-03 14:27:43 +0000

    refactor: make project abide by linter

--- lib/cone_drill_posenet/view/cone_drill_posenet.dart
+++ lib/cone_drill_posenet/view/cone_drill_posenet.dart
@@ -15,7 +15,6 @@ class ConeDrillPosenet extends StatefulWidget {
 }
 
 class _ConeDrill extends State<ConeDrillPosenet> {
-
   late CameraController controller;
   late Future<void> _initializeControllerFuture;
   double previewW = 0;
@@ -26,34 +25,47 @@ class _ConeDrill extends State<ConeDrillPosenet> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(title: Text("Cone"),),
-      body: FutureBuilder(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            return Stack(
-              children: [
-                OrientationBuilder(builder: (context, orientation) {
-                  return AspectRatio(
-                      aspectRatio: orientation == Orientation.portrait ? 1 / controller.value.aspectRatio : controller.value.aspectRatio,
-                      child: CameraPreview(controller)
-                  );
-                }),
-                Positioned(
-                    top: leftHandResult.isEmpty ? 0 : leftHandResult["y"] * previewH  - 12,
-                    left: leftHandResult.isEmpty ? 0 : leftHandResult["x"] * previewW  - 12,
-                    child: const Icon(Icons.circle, color: Colors.cyan,)
-                ),
-                Positioned(
-                    top: rightHandResult.isEmpty ? 0 : rightHandResult["y"] * previewH  - 12,
-                    left: rightHandResult.isEmpty ? 0 : rightHandResult["x"] * previewW  - 12,
-                    child: const Icon(Icons.circle, color: Colors.red,)
-                ),
-              ],
-            );
-          })
-    );
+        appBar: AppBar(
+          title: Text("Cone"),
+        ),
+        body: FutureBuilder(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              return Stack(
+                children: [
+                  OrientationBuilder(builder: (context, orientation) {
+                    return AspectRatio(
+                        aspectRatio: orientation == Orientation.portrait
+                            ? 1 / controller.value.aspectRatio
+                            : controller.value.aspectRatio,
+                        child: CameraPreview(controller));
+                  }),
+                  Positioned(
+                      top: leftHandResult.isEmpty
+                          ? 0
+                          : leftHandResult["y"] * previewH - 12,
+                      left: leftHandResult.isEmpty
+                          ? 0
+                          : leftHandResult["x"] * previewW - 12,
+                      child: const Icon(
+                        Icons.circle,
+                        color: Colors.cyan,
+                      )),
+                  Positioned(
+                      top: rightHandResult.isEmpty
+                          ? 0
+                          : rightHandResult["y"] * previewH - 12,
+                      left: rightHandResult.isEmpty
+                          ? 0
+                          : rightHandResult["x"] * previewW - 12,
+                      child: const Icon(
+                        Icons.circle,
+                        color: Colors.red,
+                      )),
+                ],
+              );
+            }));
   }
 
   @override
@@ -66,7 +78,8 @@ class _ConeDrill extends State<ConeDrillPosenet> {
 
     controller = CameraController(_cameras[0], ResolutionPreset.low);
     _initializeControllerFuture = controller.initialize().then((_) {
-      previewH = MediaQuery.of(context).size.height - 90; //TODO: Calculator size of app bar at runtime
+      previewH = MediaQuery.of(context).size.height -
+          90; //TODO: Calculator size of app bar at runtime
       previewW = previewH * controller.value.aspectRatio;
       setState(() {});
 
@@ -76,14 +89,17 @@ class _ConeDrill extends State<ConeDrillPosenet> {
         }
         isDetecting = true;
         Tflite.runPoseNetOnFrame(
-          bytesList: image.planes.map((plane) {return plane.bytes;}).toList(),
+          bytesList: image.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
           numResults: 1,
           imageHeight: image.height,
           imageWidth: image.width,
           rotation: 180,
         ).then((results) {
-          if (results!.isNotEmpty && results[0]["keypoints"][9]["score"] > 0.5
-          && results[0]["keypoints"][10]["score"] > 0.5) {
+          if (results!.isNotEmpty &&
+              results[0]["keypoints"][9]["score"] > 0.5 &&
+              results[0]["keypoints"][10]["score"] > 0.5) {
             leftHandResult = results[0]["keypoints"][9];
             rightHandResult = results[0]["keypoints"][10];
             setState(() {});
