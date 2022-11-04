6e89ace85e4691f7510f37aef3dd09349f548edd
Otto White <whiteotto4@gmail.com>
2022-11-04 10:35:26 +0000

    refactor: fix typo in mobilenet

--- lib/cone_drill_mobile_net/view/cone_drill_mobilenet.dart
+++ lib/cone_drill_mobile_net/view/cone_drill_mobilenet.dart
@@ -227,7 +227,7 @@ class _ConeDrill extends State<ConeDrillMobilenet> {
     setDrillStatus(DrillStatus.notReady);
   }
 
-  DrillStatus updateDrillStaus() {
+  DrillStatus updateDrillStatus() {
     final drillStatus = getDrillStatus();
 
     switch (drillStatus) {
