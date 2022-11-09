99d39872f4ed9927d12de6d0e55bcc11ebcdd428
Evie Wright <efw20@ic.ac.uk>
2022-11-09 22:39:34 +0000

    refactor: make changes to reflect backend api refactor

--- lib/drill_list/model/api_client_channel.dart
+++ lib/drill_list/model/api_client_channel.dart
@@ -1,7 +1,6 @@
 import 'package:grpc/grpc.dart';
 
 class ApiClientChannel {
-  //TODO: change to Google Cloud api
   static ClientChannel getClientChannel() {
     return ClientChannel(
         '0.0.0.0',
