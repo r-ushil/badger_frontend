c881564070564877b9a4a623d2192e735457b164
okarz <oka20@ic.ac.uk>
2022-11-10 19:35:17 +0000

    fix: change ip address of api client to from 0.0.0.0 to 10.0.2.2 to support running on emulator

--- lib/drill_list/model/api_client_channel.dart
+++ lib/drill_list/model/api_client_channel.dart
@@ -2,8 +2,7 @@ import 'package:grpc/grpc.dart';
 
 class ApiClientChannel {
   static ClientChannel getClientChannel() {
-    return ClientChannel(
-        '0.0.0.0',
+    return ClientChannel('10.0.2.2',
         port: 3000,
         options:
             const ChannelOptions(credentials: ChannelCredentials.insecure()));
