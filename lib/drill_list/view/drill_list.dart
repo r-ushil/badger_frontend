6928cb6a6668579c8234c05c264a76abb93aa59b
okarz <oka20@ic.ac.uk>
2022-11-15 21:03:39 +0000

    fix: remove material app creation in drill list screen

--- lib/drill_list/view/drill_list.dart
+++ lib/drill_list/view/drill_list.dart
@@ -2,26 +2,14 @@ import 'package:badger_frontend/drill_list/view-model/drill_list_view_model.dart
 import 'package:badger_frontend/drill_list/view/widgets/drill_card.dart';
 import 'package:flutter/material.dart';
 
-class DrillList extends StatelessWidget {
-  const DrillList({Key? key}) : super(key: key);
+class DrillList extends StatefulWidget {
+  const DrillList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Drill List Widget",
-      home: StatefulDrillList(),
-    );
-  }
+  State<DrillList> createState() => _DrillList();
 }
 
-class StatefulDrillList extends StatefulWidget {
-  const StatefulDrillList({super.key});
-
-  @override
-  State<StatefulDrillList> createState() => _StatefulDrillList();
-}
-
-class _StatefulDrillList extends State<StatefulDrillList> {
+class _DrillList extends State<DrillList> {
   final Future<List<DisplayableDrill>> _drillData =
       DrillViewModel.getDisplayableDrills();
 
