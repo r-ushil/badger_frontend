9beab8e46072c09305f8b63b06b8b112bf9e1029
okarz <oka20@ic.ac.uk>
2022-11-15 16:23:47 +0000

    fix: fix issue with missing google date time generated file

--- lib/api_models/drill_submission_model.dart
+++ lib/api_models/drill_submission_model.dart
@@ -1,12 +1,14 @@
 import 'package:badger_frontend/api_models/api_client_channel.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:badger_frontend/api_models/badger-api/drill_submission/v1/drill_submission_api.pbgrpc.dart';
+import 'package:badger_frontend/api_models/badger-api/google/type/datetime.pb.dart'
+    as google_date_time;
 import 'package:grpc/grpc.dart';
 
 class DrillSubmissionData {
   final String userId;
   final String drillId;
   final String bucketUrl;
-  final Timestamp timestamp;
+  final DateTime timestamp;
   final String processingStatus; //TODO: necessary?
   final int drillScore;
 
@@ -19,18 +21,30 @@ class DrillModel {
       ApiClientChannel.getClientChannel(),
       options: CallOptions(timeout: const Duration(minutes: 1)));
 
+  static DateTime convertFromGoogleDateTime(
+      google_date_time.DateTime dateTime) {
+    return DateTime(
+      dateTime.year,
+      dateTime.month,
+      dateTime.day,
+      dateTime.hours,
+      dateTime.minutes,
+      dateTime.seconds,
+    );
+  }
+
   static Future<DrillSubmissionData> getDrillSubmissionData(
       String submissionId) async {
     DrillSubmissionData drillSubmission =
-        DrillSubmissionData("0", "dummy", "dummy", Timestamp(0, 0), "dummy", 0);
+        DrillSubmissionData("0", "dummy", "dummy", DateTime(0), "dummy", 0);
     final req = GetDrillSubmissionRequest(drillSubmissionId: submissionId);
     try {
-      final res = await drillSubmissionServiceClient.getDrills(req);
+      final res = await drillSubmissionServiceClient.getDrillSubmission(req);
       drillSubmission = DrillSubmissionData(
           res.drillSubmission.userId,
           res.drillSubmission.drillId,
           res.drillSubmission.bucketUrl,
-          res.drillSubmission.timestamp,
+          convertFromGoogleDateTime(res.drillSubmission.timestamp),
           res.drillSubmission.processingStatus,
           res.drillSubmission.drillScore);
     } catch (e) {
