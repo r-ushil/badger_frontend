569d05607f0944bb4556ec6d43015bc5d37a88c3
Oli Callaghan <me@olicallaghan.com>
2022-11-16 21:01:38 +0000

    feat: connect auth to updated phone number login UI

--- lib/auth/view/auth_phone_login.dart
+++ lib/auth/view/auth_phone_login.dart
@@ -1,19 +1,30 @@
 import 'package:badger_frontend/common/auth/auth_phone_model.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
+import 'package:badger_frontend/auth/view/widgets/logo.dart';
+import 'package:badger_frontend/auth/view/widgets/header.dart';
+import 'package:badger_frontend/auth/view/widgets/textbox_with_label_and_icon.dart';
+import 'package:badger_frontend/auth/view/widgets/custom_button.dart';
 import 'package:provider/provider.dart';
 
-class AuthPhoneLogin extends StatelessWidget {
-  AuthPhoneLogin({Key? key}) : super(key: key);
+class AuthPhoneLogin extends StatefulWidget {
+  const AuthPhoneLogin({Key? key}) : super(key: key);
 
+  @override
+  State<AuthPhoneLogin> createState() => _AuthPhoneLoginState();
+}
+
+class _AuthPhoneLoginState extends State<AuthPhoneLogin> {
   final phoneNumberController = TextEditingController();
-  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     final authPhoneVerify = Provider.of<BadgerAuthPhoneModel>(context);
+
+    void onSubmitPhoneNumber() async {
+      Navigator.pushNamed(context, "/auth/phone/verify");
+
+      await authPhoneVerify.signInWithPhoneNumber(phoneNumberController.text);
+    }
 
     return GestureDetector(
       onTap: () {
@@ -25,29 +36,23 @@ class AuthPhoneLogin extends StatelessWidget {
       },
       child: Scaffold(
         appBar: AppBar(),
-        body: Form(
+        body: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              const Text('Login with Phone Number'),
-              TextFormField(
-                controller: phoneNumberController,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(labelText: 'Phone Number'),
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  PhoneInputFormatter(),
+              const Center(child: Logo()),
+              const Header("WELCOME"),
+              const SizedBox(height: 50), // margin welcome text
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextBoxWithLabelAndIcon(
+                      "Phone Number", Icons.phone, phoneNumberController),
                 ],
-                style: const TextStyle(fontSize: 18.0, color: Colors.white),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  Navigator.pushNamed(context, "/auth/phone/verify");
-
-                  await authPhoneVerify
-                      .signInWithPhoneNumber(phoneNumberController.text);
-                },
-                child: const Text('Login'),
+              const SizedBox(height: 20),
+              CustomButton(
+                "Login",
+                onSubmitPhoneNumber,
               ),
             ],
           ),
