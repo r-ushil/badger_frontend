09adc3fc2e0807397cfeef7d781f0b308a6139e1
Oli Callaghan <me@olicallaghan.com>
2022-11-16 21:02:00 +0000

    feat: switch OTP verification screen to new UI

--- lib/auth/view/auth_phone_verify.dart
+++ lib/auth/view/auth_phone_verify.dart
@@ -1,44 +1,76 @@
+import 'package:badger_frontend/common/auth/auth_phone_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:badger_frontend/auth/view/widgets/custom_button.dart';
+import 'package:flutter_verification_code/flutter_verification_code.dart';
 import 'package:provider/provider.dart';
 
-import '../../common/auth/auth_phone_model.dart';
+class AuthPhoneVerify extends StatefulWidget {
+  const AuthPhoneVerify({Key? key}) : super(key: key);
 
-class AuthPhoneVerify extends StatelessWidget {
-  AuthPhoneVerify({Key? key}) : super(key: key);
+  @override
+  State<AuthPhoneVerify> createState() => _AuthPhoneVerifyState();
+}
 
-  final otpController = TextEditingController();
+class _AuthPhoneVerifyState extends State<AuthPhoneVerify> {
+  String? _verificationCode;
 
   @override
   Widget build(BuildContext context) {
     final authPhoneVerify = Provider.of<BadgerAuthPhoneModel>(context);
 
+    void onSubmitOtp() async {
+      String? otp = _verificationCode;
+      if (otp == null) return;
+
+      await authPhoneVerify.verifyPhoneNumberWithOtp(otp);
+    }
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Login with Phone Number")),
-      body: Form(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text('Verify Phone Number with OTP'),
-            TextFormField(
-              controller: otpController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'OTP Code'),
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+            const Text("Verify your Number \n"),
+            const Text(
+              "A 6-digit code is sent to you.",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const Padding(
+                padding: EdgeInsets.all(2),
+                child: Text(
+                  "Please confirm your account by entering the code.",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                )),
+            const SizedBox(height: 30), // margin
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                VerificationCode(
+                    length: 6,
+                    underlineColor: Colors.green,
+                    underlineWidth: 2,
+                    fullBorder: true,
+                    autofocus: true,
+                    textStyle: const TextStyle(color: Colors.white),
+                    onCompleted: (String valueRead) {
+                      _verificationCode = valueRead;
+                      setState(() {});
+                    },
+                    onEditing: (bool isCurrentlyEditing) {
+                      if (!isCurrentlyEditing) {
+                        FocusScope.of(context).unfocus();
+                      }
+                    }),
               ],
-              style: const TextStyle(fontSize: 18.0, color: Colors.white),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                await authPhoneVerify
-                    .verifyPhoneNumberWithOtp(otpController.text);
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        ),
-      ),
+            const SizedBox(height: 30),
+            CustomButton("Verify", onSubmitOtp),
+          ])),
     );
   }
 }
