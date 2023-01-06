f9524c9e9049edbdfd5ea14aa3e269d55d6fd516
Evie Wright <efw20@ic.ac.uk>
2023-01-06 14:32:04 +0000

    feat: changed metric chart to use a future builder (*initial before checkout: requires testing*)

--- lib/dashboard/view/widgets/metric_chart.dart
+++ lib/dashboard/view/widgets/metric_chart.dart
@@ -3,36 +3,58 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
-class MetricChart extends StatelessWidget {
-  const MetricChart({Key? key}) : super(key: key);
+class MetricChart extends StatefulWidget {
+  const MetricChart({super.key});
 
+  @override
+  State<MetricChart> createState() => _MetricChart();
+}
+
+class _MetricChart extends State<MetricChart> {
   @override
   Widget build(BuildContext context) {
     final dashboardViewModel = Provider.of<DashboardViewModel>(context);
-    final metricData = DashboardViewModel.metrics;
+    final metricData = DashboardViewModel.getMetrics();
     final profpic = dashboardViewModel.getProfilePicture();
 
-    return SfCircularChart(series: <CircularSeries>[
-      // Render pie chart
-      DoughnutSeries<MetricData, String>(
-          dataSource: metricData,
-          xValueMapper: (MetricData data, _) => data.name,
-          yValueMapper: (MetricData data, _) => data.score,
-          innerRadius: '70%',
-          explode: false,
-          dataLabelMapper: (MetricData data, _) => "${data.score}",
-          pointColorMapper: (MetricData data, _) => data.color,
-          dataLabelSettings: const DataLabelSettings(
-            isVisible: true,
-            labelPosition: ChartDataLabelPosition.outside,
-            useSeriesColor: true,
-            textStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          ),
-          animationDuration: 1000)
-    ], annotations: <CircularChartAnnotation>[
-      CircularChartAnnotation(
-        widget: profpic,
-      )
-    ]);
+    return FutureBuilder<List<MetricData>>(
+        future: metricData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SfCircularChart(series: <CircularSeries>[
+              // Render pie chart
+              DoughnutSeries<MetricData, String>(
+                  dataSource: snapshot.data,
+                  xValueMapper: (MetricData data, _) => data.name,
+                  yValueMapper: (MetricData data, _) => data.score,
+                  innerRadius: '70%',
+                  explode: false,
+                  dataLabelMapper: (MetricData data, _) => "${data.score}",
+                  pointColorMapper: (MetricData data, _) => data.color,
+                  dataLabelSettings: const DataLabelSettings(
+                    isVisible: true,
+                    labelPosition: ChartDataLabelPosition.outside,
+                    useSeriesColor: true,
+                    textStyle:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  animationDuration: 1000)
+            ], annotations: <CircularChartAnnotation>[
+              CircularChartAnnotation(
+                widget: profpic,
+              )
+            ]);
+          }
+          return Center(
+            child: Column(
+              children: const <Widget>[
+              SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(color: Colors.white)),
+              ],
+            ),
+          );
+        });
   }
 }
