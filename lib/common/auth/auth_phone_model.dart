9ff43d3fc19c76692ec4b1de37a533a0f7f67db5
Evie Wright <efw20@ic.ac.uk>
2023-01-07 11:35:46 +0000

    feat: add code to insert new user into people database

--- lib/common/auth/auth_phone_model.dart
+++ lib/common/auth/auth_phone_model.dart
@@ -1,4 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:badger_frontend/api_models/badger-api/person/v1/person_api.pbgrpc.dart'
+    as person_api;
+import 'package:badger_frontend/api_models/api_client_channel.dart' as client_channel;
+import 'package:grpc/grpc.dart';
 
 enum BadgerAuthPhoneState {
   waitingForPhoneNumber,
@@ -35,7 +39,19 @@ class BadgerAuthPhoneModel {
       smsCode: otp,
     );
 
-    await auth.signInWithCredential(credential);
+    await auth.signInWithCredential(credential).then((credential) async {
+          if (credential.additionalUserInfo!.isNewUser) { 
+            final personServiceClient = person_api.PersonServiceClient(
+                client_channel.ApiClientChannel.getClientChannel(),
+                options: CallOptions(timeout: const Duration(minutes: 1)));
+            try {
+              var res = await personServiceClient.insertNewUser(person_api.InsertNewUserRequest(firebaseId: credential.user!.uid));
+              return res.hexId;
+            } catch (e) {
+              rethrow;
+              //TODO: error handling
+            }
+    }});
 
     _verificationId = null;
   }
