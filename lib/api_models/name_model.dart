54c37ac737af479e3ad3a81f941e0a9fbbb97371
r-ushil <rushil.patel791@gmail.com>
2023-01-12 03:41:52 +0000

    feat: added name grpc endpoint

---
+++ lib/api_models/name_model.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:grpc/grpc.dart';
+
+import 'api_client_channel.dart';
+import 'badger-api/leaderboard/v1/leaderboard_api.pbgrpc.dart';
+
+class NameModel {
+  static final nameServiceClient = LeaderboardServiceClient(
+      ApiClientChannel.getClientChannel(),
+      options: CallOptions(timeout: const Duration(minutes: 1)));
+
+  static Future<String> getName() async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+
+    final req = GetMyPublicNameRequest();
+    try {
+      final res = await nameServiceClient.getMyPublicName(req,
+          options: CallOptions(metadata: {"authorization": uid}));
+      return res.name;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  static setName(String name) async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+
+    final req = SetMyPublicNameRequest(name);
+    try {
+      await nameServiceClient.setMyPublicName(req,
+          options: CallOptions(metadata: {"authorization": uid}));
+    } catch (e) {
+      rethrow;
+    }
+  }  
+  
+}
