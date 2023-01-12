55acb6e6fbaa8a8672c4660e9ce67c696f9c473c
r-ushil <rushil.patel791@gmail.com>
2023-01-12 03:19:35 +0000

    feat: added blank leaderboard page

--- lib/dashboard/leaderboard.dart
+++ lib/dashboard/leaderboard.dart
@@ -1,22 +1,22 @@
-import 'package:badger_frontend/common/auth/auth_model.dart';
-import 'package:badger_frontend/dashboard/leaderboard.dart';
-import 'package:badger_frontend/dashboard/view/widgets/metric_chart.dart';
-import 'package:badger_frontend/dashboard/view/widgets/progress_bars.dart';
+import 'package:badger_frontend/dashboard/view/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../drill_list/view/drill_list.dart';
+import '../common/auth/auth_model.dart';
+import '../drill_list/view/drill_list.dart';
 
-class Dashboard extends StatefulWidget {
-  const Dashboard({Key? key}) : super(key: key);
+class Leaderboard extends StatefulWidget {
+  const Leaderboard({Key? key}) : super(key: key);
 
   @override
-  State<Dashboard> createState() => _DashboardState();
+  State<Leaderboard> createState() => _LeaderboardState();
 }
 
-class _DashboardState extends State<Dashboard> {
+class _LeaderboardState extends State<Leaderboard> {
+
   @override
   Widget build(BuildContext context) {
+
     final auth = Provider.of<BadgerAuth>(context);
 
     return Scaffold(
@@ -29,13 +29,15 @@ class _DashboardState extends State<Dashboard> {
                     margin: const EdgeInsets.only(left: 10, top: 50),
                     width: (MediaQuery.of(context).size.width - 20) / 2,
                     child: TextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
                         style: TextButton.styleFrom(
                           shape: const RoundedRectangleBorder(
                               borderRadius: BorderRadius.only(
                                   topLeft: Radius.circular(10),
                                   bottomLeft: Radius.circular(10))),
-                          backgroundColor: Colors.green.withOpacity(0.5),
+                          backgroundColor: Colors.green,
                           foregroundColor: Colors.white,
                         ),
                         child: const Text("Overview"))),
@@ -43,27 +45,20 @@ class _DashboardState extends State<Dashboard> {
                     margin: const EdgeInsets.only(right: 10, top: 50),
                     width: (MediaQuery.of(context).size.width - 20) / 2,
                     child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Leaderboard()));
-                        },
+                        onPressed: () {},
                         style: TextButton.styleFrom(
                             shape: const RoundedRectangleBorder(
                                 side: BorderSide(color: Colors.green, width: 3),
                                 borderRadius: BorderRadius.only(
                                     topRight: Radius.circular(10),
                                     bottomRight: Radius.circular(10))),
-                            backgroundColor: Colors.green,
+                            backgroundColor: Colors.green.withOpacity(0.5),
                             foregroundColor: Colors.white),
                         child: const Text("Leaderboard"))),
               ],
             ),
-            const MetricChart(),
-            const Padding(
-                padding: EdgeInsets.only(left: 40.0),
-                child: MetricProgressBars()),
+            // add list for leaderboard here
+            
           ]),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add_circle, size: 60),
