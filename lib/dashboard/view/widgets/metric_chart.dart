354e6517e2aa37c9ef5ec2e38e941e91f69b38cc
r-ushil <rushil.patel791@gmail.com>
2023-01-12 02:32:40 +0000

    feat: added placeholder for 0 submissions

--- lib/dashboard/view/widgets/metric_chart.dart
+++ lib/dashboard/view/widgets/metric_chart.dart
@@ -14,34 +14,53 @@ class _MetricChart extends State<MetricChart> {
   Widget build(BuildContext context) {
     final metrics = DashboardViewModel.getMetrics();
 
-
     return FutureBuilder<List<MetricData>>(
         future: metrics,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return SfCircularChart(series: <CircularSeries>[
-              // Render pie chart
-              DoughnutSeries<MetricData, String>(
-                  dataSource: snapshot.data,
-                  xValueMapper: (MetricData data, _) => data.name,
-                  yValueMapper: (MetricData data, _) => data.score,
-                  innerRadius: '70%',
-                  explode: false,
-                  dataLabelMapper: (MetricData data, _) => "${data.score}",
-                  pointColorMapper: (MetricData data, _) => data.color,
-                  dataLabelSettings: const DataLabelSettings(
-                    isVisible: true,
-                    labelPosition: ChartDataLabelPosition.outside,
-                    useSeriesColor: true,
-                    textStyle:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  animationDuration: 1000)
-            ], annotations: <CircularChartAnnotation>[
-              CircularChartAnnotation(
-                widget: Text(getTotalSubmissions(snapshot.data!).toString()),
-              )
-            ]);
+            int totalSubmissions =
+                getTotalSubmissions(snapshot.data!);
+
+            if (totalSubmissions == 0) {
+              return Center(
+                child: Column(
+                  children: const <Widget>[
+                    SizedBox(
+                        width: 60,
+                        height: 60),
+                    Text(
+                      "Complete you first drill to see some submission statistics!", textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
+                    SizedBox(
+                        width: 60,
+                        height: 250),
+                  ],
+                ),
+              );
+            } else {
+              return SfCircularChart(series: <CircularSeries>[
+                // Render pie chart
+                DoughnutSeries<MetricData, String>(
+                    dataSource: snapshot.data,
+                    xValueMapper: (MetricData data, _) => data.name,
+                    yValueMapper: (MetricData data, _) => data.score,
+                    innerRadius: '70%',
+                    explode: false,
+                    dataLabelMapper: (MetricData data, _) => "${data.score}",
+                    pointColorMapper: (MetricData data, _) => data.color,
+                    dataLabelSettings: const DataLabelSettings(
+                      isVisible: true,
+                      labelPosition: ChartDataLabelPosition.outside,
+                      useSeriesColor: true,
+                      textStyle:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    animationDuration: 1000)
+              ], annotations: <CircularChartAnnotation>[
+                CircularChartAnnotation(
+                  widget: Text(totalSubmissions.toString()),
+                )
+              ]);
+            }
           }
           return Center(
             child: Column(
@@ -57,7 +76,7 @@ class _MetricChart extends State<MetricChart> {
   }
 }
 
-getTotalSubmissions(List<MetricData> metrics) {
+int getTotalSubmissions(List<MetricData> metrics) {
   int total = 0;
   for (final metric in metrics) {
     total += metric.submissions;
